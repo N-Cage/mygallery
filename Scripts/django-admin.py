88012ffe3f7800d364722889ce8ec83ac41572e5
unknown <nicholascage0149@gmail.com>
2022-04-10 19:33:37 +0300

    git config --global core.editor "code --wait"

---
+++ Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\hp\desktop\learnings\programming\python\scripts_and_shit\a_liubiiev\image_gallery\image_gallery\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
---
+++ image_gallery/urls.py
@@ -0,0 +1,48 @@
+"""image_gallery URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from pages.views import(
+    home_page_view,
+    upload_picture_view,
+    must_authenticate_view,
+    gallery_view,
+)
+
+from account.views import(
+    registration_view,
+    logout_view,
+    login_view,
+    )
+
+urlpatterns = [
+    path('', home_page_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('gallery/', gallery_view, name='gallery'),
+    path('must_authenticate/', must_authenticate_view, name='must_auth'),
+    path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('upload/', upload_picture_view, name='upload_picture'),
+    path('register/', registration_view, name='register'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
